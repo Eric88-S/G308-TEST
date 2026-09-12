49b852af0575c2cfe80afdfb9fa9212a20e5fffc
Eric88-S <3020562310@qq.com>
2026-09-12 16:49:22 +0800

    Add comments for clarity in create_grid.py

--- route planning/create_grid.py
+++ route planning/create_grid.py
@@ -2,29 +2,29 @@ import random
 import numpy as np
 
 
-class grid:
+class grid:#创建类
     def __init__(self,rows,cols,obstacle):
         self.rows = rows
         self.cols = cols
         self.obstacle = obstacle
         self.array = np.zeros((rows,cols),dtype=int)
         for x,y in obstacle:
-            self.array[x,y]=1
+            self.array[x,y]=1#让障碍物为1方便可视化
 
-    def neighbors(self,current):
+    def neighbors(self,current):#遍历周围上下左右的格子
         x,y=current
-        neighbors = [(1,0),(0,1),(-1,0),(0,-1)]
+        neighbors = [(1,0),(0,1),(-1,0),(0,-1)]#当前基础上的行动范围    
         next=[]
         for dx,dy in neighbors:
             nx=x+dx
             ny=y+dy
             if 0 <= nx < self.rows and 0 <= ny < self.cols:
                 if (nx,ny) not in self.obstacle:
-                    next.append((nx,ny))
+                    next.append((nx,ny))#找可能有的路径
         return next
 
     def copy(self):
-        return self.array.copy()
+        return self.array.copy()#复制地图
 
 
 def create_grid(rows:int,cols:int,obstacle_rate=0.3,start=(0,0),end=None):
@@ -32,13 +32,13 @@ def create_grid(rows:int,cols:int,obstacle_rate=0.3,start=(0,0),end=None):
         end=(rows-1,cols-1)
     array=np.zeros((rows,cols),dtype=int)
     total_cells=rows*cols
-    obstacle_count=int(total_cells*obstacle_rate)
+    obstacle_count=int(total_cells*obstacle_rate)#障碍数
 
 
 
     forbidden_cell={start,end}
     all_points=[(i,j) for i in range(rows) for j in range(cols) if (i,j)not in forbidden_cell]
-    obs_pos=random.sample(all_points,obstacle_count)
+    obs_pos=random.sample(all_points,obstacle_count)#随机选障碍数
 
     return grid(rows,cols,set(obs_pos))
 
